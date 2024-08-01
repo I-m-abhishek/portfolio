7f1f329943a8d428f75165872388182a2321561e
Abhishek katiyar <akatiyar0987@gmail.com>
2024-08-01 12:15:27 +0530

    Hero Section Completed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
+// next.config.mjs
+// next.config.mjs
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/files',
+          outputPath: 'static/files',
+        },
+      },
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
