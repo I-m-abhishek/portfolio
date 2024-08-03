3316d3a8b201b1f2a449ef720200fc09127de7fb
Abhishek katiyar <akatiyar0987@gmail.com>
2024-08-03 20:20:37 +0530

    About section done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,64 @@
+"use client";
 import React from 'react'
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn ,textVariant } from '../utils/motion'
+import { SectionWrapper } from '../hoc';
 
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon.src}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+    </motion.div>
+    
+    <motion.p 
+    variants={fadeIn("","",0.1 , 1)}
+    className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+    >
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas dolore, omnis laborum, repudiandae minus et facilis aut adipisci non porro consectetur nostrum nulla tempore molestias beatae atque hic alias fugiat fugit. Quas, veritatis. Vero rerum quia architecto cum, dolores nobis voluptate pariatur et maxime voluptas. Distinctio dolorum praesentium aperiam. Possimus facere animi necessitatibus laudantium porro consequuntur?
+    </motion.p>
+    
+    <div className='mt-20 flex flex-wrap gap-10'>
+
+{services.map((service, index)=>{
+  return(
+<ServiceCard  key={service.title} index={index} {...service}/>
+  )
+})}
+    </div>
+    </>
   )
 }
 
-export default About
+export default SectionWrapper(About , "about")
