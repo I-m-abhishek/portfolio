d52e6e4845e4a029320996f0798e0abfc678b6c3
Abhishek katiyar <akatiyar0987@gmail.com>
2024-09-02 14:40:05 +0530

    Frontend Completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+"use client";
+import{useState, useRef} from 'react';
+import { motion } from 'framer-motion';
+import emailjs from '@emailjs/browser';
+import { styles } from '../styles';
+import { EarthCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { slideIn } from '../utils/motion';
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name : '',
+    email:'',
+    message: '',
+  });
+  const [loading, setLoading] = useState(false);
+  const handlechange = (e) =>{ 
+   
+  }
+  const handlesubmit = (e)=>{
+
+  }
   return (
-    <div>Contact</div>
+    <>
+    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
+    <motion.div variants={slideIn('left' , "tween" , 0.2 ,1)}
+    className='flex-[0.75] bg-black-100 p-8  rounded-2xl'>
+<p className={`${styles.sectionSubText}`}>Get in touch</p>
+<h3 className={`${styles.sectionHeadText}`}>Contact.</h3>
+
+ <form 
+  ref={formRef}
+  onSubmit={handlesubmit}
+  className='mt-12 flex flex-col gap-8'
+ >
+     <label className='flex flex-col'>
+      <span className='text-white font-medium mb-4'>Your Name</span>
+      <input className='px-6 bg-tertiary py-4 placeholder:text-secondary text-white rounded-lg outlined-none border-none ' type="text" name='name' value={form.name} onChange={handlechange} placeholder="What's your name"/>
+     </label>
+     <label className='flex flex-col'>
+      <span className='text-white font-medium mb-4'>Your Email</span>
+      <input className='px-6 bg-tertiary py-4 placeholder:text-secondary text-white rounded-lg outlined-none border-none ' type="email" name='email' value={form.email} onChange={handlechange} placeholder="What's your email"/>
+     </label>
+     <label className='flex flex-col'>
+      <span className='text-white font-medium mb-4'>Message</span>
+      <textarea className='px-6 bg-tertiary py-4 placeholder:text-secondary text-white rounded-lg outlined-none border-none ' rows="7"  name='message' value={form.message} onChange={handlechange} placeholder="What do you want to say?"/>
+     </label>
+     <button type='submit' className='bg-tertiary py-3 px-8 outlined-none font-bold w-fit text-white shadow-md shadow-primary rounded-xl'>
+      {loading ? 'Sending...' : "Send"}
+     </button>
+ </form>
+    </motion.div>
+    <motion.div variants={slideIn('right' , "tween" , 0.2 , 1 )} className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'>
+ <EarthCanvas/>
+
+    </motion.div>
+    </div>
+    </>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact ,"contact");
--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -3,9 +3,19 @@ import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { staggerContainer } from '../utils/motion';
 
-const SectionWrapper = ( Component , idNamw)=>function HOC() {
+// video at 1hr 22 min-  https://youtu.be/0fYi8SGA20k?si=04NrsqVRuv2dldDZ
+const SectionWrapper = ( Component , idName)=>function HOC() {
   return (
-    <motion.section>
+    <motion.section
+    variants={staggerContainer()}
+    initial="hidden"
+    whileInView="show"
+    viewport={{once: true , amount : 0.25}}
+    className={` ${styles.padding} max-w-7xl mx-auto relative z-0`}
+    >
+      <span className='hash-span' id={idName}>
+        &nbsp;
+      </span>
         <Component/>
     </motion.section>
   )
